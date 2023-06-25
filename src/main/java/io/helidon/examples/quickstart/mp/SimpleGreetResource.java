2507fc2d3549c14054ac8500c46cdab587b691d7
extact-io <64010485+extact-io@users.noreply.github.com>
2023-06-25 08:22:01 +0000

    update import

--- src/main/java/io/helidon/examples/quickstart/mp/SimpleGreetResource.java
+++ src/main/java/io/helidon/examples/quickstart/mp/SimpleGreetResource.java
@@ -1,21 +1,16 @@
-
 package io.helidon.examples.quickstart.mp;
 
-import java.util.Collections;
-
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
-import jakarta.ws.rs.PathParam;
 
-import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 /**
  * A simple JAX-RS resource to greet you. Examples:
